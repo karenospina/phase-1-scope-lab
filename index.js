abd3dda652c68045d170cc5b8cc4808e4776dab2
+ <+>
2024-01-08 21:42:40 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Write your solution in this file!
+var customerName = 'bob';
+var bestCustomer;
+
+
+function upperCaseCustomerName() {
+        console.log(customerName.toUpperCase());
+    }
+    
+upperCaseCustomerName();
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+ setBestCustomer();
+ console.log(bestCustomer);
+
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+overwriteBestCustomer();
+console.log(bestCustomer);
+
+
+const leastFavoriteCustomer = 'Lucy';
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'Tom';
+    
+}
+
+changeLeastFavoriteCustomer();
+console.log(leastFavoriteCustomer);
